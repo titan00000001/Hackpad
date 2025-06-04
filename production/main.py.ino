bfe8adbee3d376f35a928e5dbff04b53538276e2
titan00000001 <105803076+titan00000001@users.noreply.github.com>
2025-06-04 16:16:15 -0500

    Add files via upload

---
+++ production/main.py.ino
@@ -0,0 +1,26 @@
+import board
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.scanners.keypad import KeysScanner
+from kmk.keys import KC
+from kmk.modules.macros import Press, Release, Tap, Macros
+
+keyboard = KMKKeyboard()
+
+macros = Macros()
+keyboard.modules.append(macros)
+
+# Define your pins here!
+PINS = [board.D3, board.D4, board.D2, board.D1]
+
+keyboard.matrix = KeysScanner(
+    pins=PINS,
+    value_when_pressed=False,
+)
+
+
+keyboard.keymap = [
+    [KC.UP, KC.DOWN, KC.LEFT, KC.RIGHT]
+]
+
+if __name__ == '__main__':
+    keyboard.go()
